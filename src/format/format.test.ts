a44bf2d527c8d007a1226756e52e7298d69dbab4
WilcoSp <wilcosp@hotmail.nl>
2024-04-05 23:20:05 +0200

    added tests for format

--- src/format/format.test.ts
+++ src/format/format.test.ts
@@ -4,6 +4,7 @@ import { format } from "./format";
 
 describe("format", () => {
 	suite("en-US", () => {
+		const locale = "en-US";
 		it("should give 5 days & 3 seconds", () => {
 			expect(
 				format(
@@ -11,7 +12,7 @@ describe("format", () => {
 					{
 						style: "long",
 					},
-					"en-US"
+					locale
 				)
 			).toBe("5 days 3 seconds");
 		});
@@ -23,7 +24,7 @@ describe("format", () => {
 					{
 						style: "narrow",
 					},
-					"en-US"
+					locale
 				)
 			).toBe("-3h 58m");
 		});
@@ -36,9 +37,89 @@ describe("format", () => {
 						style: "short",
 						max: 2,
 					},
-					"en-US"
+					locale
 				)
 			).toBe("6 yr. 10 wk.");
+		});
+	});
+
+	suite("main Chinese", () => {
+		const locale = "zh-CH";
+		it("should give 5 days & 3 seconds", () => {
+			expect(
+				format(
+					day * 5 + second * 3,
+					{
+						style: "long",
+					},
+					locale
+				)
+			).toBe("5天 3秒钟");
+		});
+
+		it("should give -3 hours 58 minutes in narrow form", () => {
+			expect(
+				format(
+					-(3 * hour + 58 * minute),
+					{
+						style: "narrow",
+					},
+					locale
+				)
+			).toBe("-3小时 58分钟");
+		});
+
+		it("should give 6 years, 10 weeks but not 4 days", () => {
+			expect(
+				format(
+					year * 6 + 10 * week + 4 * day,
+					{
+						style: "short",
+						max: 2,
+					},
+					locale
+				)
+			).toBe("6年 10周");
+		});
+	});
+
+	suite("Hong Kong Chinese", () => {
+		const locale = "zh-HK";
+		it("should give 5 days & 3 seconds", () => {
+			expect(
+				format(
+					day * 5 + second * 3,
+					{
+						style: "long",
+					},
+					locale
+				)
+			).toBe("5 日 3 秒");
+		});
+
+		it("should give -3 hours 58 minutes in narrow form", () => {
+			expect(
+				format(
+					-(3 * hour + 58 * minute),
+					{
+						style: "narrow",
+					},
+					locale
+				)
+			).toBe("-3小時後 58分後");
+		});
+
+		it("should give 6 years, 10 weeks but not 4 days", () => {
+			expect(
+				format(
+					year * 6 + 10 * week + 4 * day,
+					{
+						style: "short",
+						max: 2,
+					},
+					locale
+				)
+			).toBe("6 年 10 星期");
 		});
 	});
 });
